bf64f38438fb8aac7a3d24f82714eaed03176349
Your Name <you@example.com>
2018-08-26 12:11:54 +1200

    Updated Green & Orange

--- events.js
+++ events.js
@@ -7,7 +7,9 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+  two()
+  three()
+  four()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -23,10 +25,25 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.addEventListener('mouseenter', makeGreen)
+  two.addEventListener('mouseleave', makeWhite)
+}
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener('mouseleave', makeWhite)
+}
 
 // CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four')
+  four.addEventListener('mouseenter', makeBlue)
+  four.addEventListener('mouseleave', makeWhite)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -35,4 +52,11 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
